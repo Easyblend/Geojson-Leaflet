3a0371dd9a5fe2c452a7a99ea57574bd1507a94f
easyblend <kenzieemma072@gmail.com>
2024-02-26 09:14:49 +0100

    extra style to map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useRef } from 'react';
+
+// Import necessary dependencies
+import { useEffect, useRef } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl, { LngLatLike, Map } from 'mapbox-gl';
 import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
 import { datas } from './data/geoData';
-import { zipCodes } from './data/datag';
-const mapStyle = 'mapbox://styles/mapbox/streets-v11';
+
+// Define map style and Mapbox access token
+const mapStyle = 'mapbox://styles/ekumah/clt02krq400fv01pi4iv9elan';
 const mapboxToken = 'pk.eyJ1IjoiZWt1bWFoIiwiYSI6ImNsc3gxczhnbjJtZmgya25zeHNhcXo1bzcifQ.B3ISBLaHVL1WSt8w5MXPvw';
 
+// Define interface for a feature on the map
 interface PlaceFeature {
     type: 'Feature';
     properties: {
@@ -20,12 +24,16 @@ interface PlaceFeature {
     };
 }
 
+// Maps component
 function Maps(): JSX.Element {
+    // Reference to the map container element
     const mapContainer = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        // Set Mapbox access token
         mapboxgl.accessToken = mapboxToken;
 
+        // Create a new Mapbox map instance
         const map = new mapboxgl.Map({
             container: mapContainer.current!,
             style: mapStyle,
@@ -33,31 +41,33 @@ function Maps(): JSX.Element {
             zoom: 11
         });
 
+        // Initialize map when it's loaded
         map.on('load', () => {
-            // Convert zipCodes to GeoJSON
-
+            // Add GeoJSON source to the map
             map.addSource('places', {
                 type: 'geojson',
                 data: datas
             });
 
+            // Add a layer to display features from the GeoJSON source
             map.addLayer({
                 id: 'places',
-                type: 'circle', // Change the layer type to 'circle'
+                type: 'circle',
                 source: 'places',
                 paint: {
-                    'circle-color': '#4264fb', // Fix: Change 'Point-color' to 'circle-color'
+                    'circle-color': '#4264fb',
                     'circle-opacity': 0.9,
                     'circle-radius': 8
-                    // Optional: Add other properties as needed
                 }
             });
 
+            // Create a popup for displaying feature information
             const popup = new mapboxgl.Popup({
                 closeButton: false,
                 closeOnClick: false
             });
 
+            // Event listener for mouseenter event on map features
             map.on('mouseenter', 'places', (e) => {
                 map.getCanvas().style.cursor = 'pointer';
                 const feature = e.features?.[0] as PlaceFeature | undefined;
@@ -65,30 +75,33 @@ function Maps(): JSX.Element {
                     const coordinates = feature.geometry.coordinates;
                     const description = feature.properties.description;
                     const image = feature.properties.image;
-                    console.log(feature.properties.image);
 
+                    // Set popup content and display it on the map
                     popup.setLngLat(coordinates).setHTML(
                         `<div>
-                <p>${description}</p>
-                <div height="100px" width="100px" >${image}</div>
-            </div>`
+                            <p>${description}</p>
+                            <div height="100px" width="100px">${image}</div>
+                        </div>`
                     ).addTo(map);
                 }
             });
 
-
+            // Event listener for mouseleave event on map features
             map.on('mouseleave', 'places', () => {
                 map.getCanvas().style.cursor = '';
                 popup.remove();
             });
         });
 
+        // Cleanup function to remove the map instance when component unmounts
         return () => map.remove();
     }, []);
 
+    // Render the map container element
     return <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />;
 }
 
+// Function to convert FeatureCollection to GeoJSON format
 function convertToGeoJSON(featureCollection: FeatureCollection<Geometry, GeoJsonProperties>) {
     const geoJsonData: FeatureCollection = {
         type: 'FeatureCollection',
@@ -101,7 +114,21 @@ function convertToGeoJSON(featureCollection: FeatureCollection<Geometry, GeoJson
     return geoJsonData;
 }
 
+// Export the Maps component as default
 export default Maps;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
